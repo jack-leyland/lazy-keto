5a621ac3d958a774a85bcf49849ca006e8d3e9b1
jack-leyland <jackjleyland@gmail.com>
2020-10-06 17:11:48 -0400

    clean up old  comments

--- src/assets/icons/GenerateIcon.js
+++ src/assets/icons/GenerateIcon.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function GenerateIcon(props) {
-  // a viewbox height/width is passed. Allows for more sizing options and ensure icon viewbox is always a square
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
