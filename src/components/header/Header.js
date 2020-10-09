e24efa02fab5048b5302349f5e079de39f32b003
jack-leyland <jackjleyland@gmail.com>
2020-10-09 11:26:53 -0400

    header skeleton, basic styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Header.module.scss';
 import {
   Generate as GenerateIcon,
-  Recipe as RecipesIcon,
+  Recipe as RecipeIcon,
   Resources as ResourcesIcon,
 } from '../../assets/index';
 import { Link } from 'react-router-dom';
@@ -10,7 +10,37 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper}></div>
+      <Logo />
+      <Nav />
+    </div>
+  );
+}
+
+function Logo() {
+  return <div className={styles.logo}>Lazy Keto</div>;
+}
+
+function Nav() {
+  return (
+    <div className={styles.nav}>
+      <Link to="/recipes">
+        <div className={styles['recipes-button']}>
+          <RecipeIcon />
+          <div>Recipes</div>
+        </div>
+      </Link>
+      <Link to="/generate">
+        <div className={styles['generate-button']}>
+          <GenerateIcon />
+          <div>Generate Meal</div>
+        </div>
+      </Link>
+      <Link to="/resources">
+        <div className={styles['resources-button']}>
+          <ResourcesIcon />
+          <div>Resources</div>
+        </div>
+      </Link>
     </div>
   );
 }
